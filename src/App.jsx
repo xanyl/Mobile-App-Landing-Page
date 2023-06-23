5012ada3165efb462691c55dcfcc17015133f84b
xanyl <aniltiwari645@gmail.com>
2023-06-23 16:06:18 +0545

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div style={{ width: "100%" ,
-    outline : '1px solid red'}}>
+    <div style={{ width: "100%" }}>
       <LandingPage />
     </div>
   );
